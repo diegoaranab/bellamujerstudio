b7d21ec5ee32553e60baaedbb043a12eceb84575
diegoaranab <77321344+diegoaranab@users.noreply.github.com>
2026-01-14 22:22:07 -0600

    Create Bella Mujer Studio MVP shell

---
+++ src/app/pages/inventario/inventario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-inventario',
+  imports: [MatCardModule],
+  templateUrl: './inventario.component.html',
+  styleUrl: './inventario.component.scss'
+})
+export class InventarioComponent {}
